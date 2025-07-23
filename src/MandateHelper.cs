8bdf320090b8b3986663c2bf166227f41b78afb7
RundownRhino <52856631+RundownRhino@users.noreply.github.com>
2025-07-23 17:36:05 +0300

    Try in vain to get a custom lua function to work

--- src/MandateHelper.cs
+++ src/MandateHelper.cs
@@ -1,4 +1,64 @@
-namespace MandateHelper;
-public class MandateHelper {
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using HarmonyLib;
+using XiaWorld;
+using static MandateHelper.Utils;
 
+namespace MandateHelper;
+// Make that mandate hook exposed to Lua:
+[XLua.LuaCallCSharp]
+public class MandateHelper {
+    public static void OnInit() {
+        PatchStories();
+    }
+    private static void Info(string fmt, params object[] args) {
+        KLog.Info($"[{nameof(MandateHelper)}] {fmt}", args);
+    }
+    private static void Warn(string fmt, params object[] args) {
+        KLog.Warn($"[{nameof(MandateHelper)}] {fmt}", args);
+    }
+
+    public static Regex LawMatchRegex = new(@"GetMatchingWithGong\(['""](.+?)['""]\)");
+    public static void PatchStories() {
+        var m_mapStories = Traverse.Create(typeof(MapStoryMgr)).Field("m_mapStories").GetValue<Dictionary<string, MapStoryDef>>();
+        foreach (var kv in m_mapStories) {
+            var sdef = kv.Value;
+            var sname = sdef.Name;
+            // foreach (var sel in sdef.Selections) {
+            //     KLog.Info(sel.Display);
+            //     KLog.Info(sel.Condition);
+            //     KLog.Info(sel.OKResult);
+            // }
+            if (!sname.StartsWith("Secrets_Gong")) {
+                continue;
+            }
+            Info("Patching mandate story '{0}'", sname);
+            var acceptChoice = sdef.Selections.Find(x => x.OKResult?.Contains("GetMatchingWithGong") ?? false);
+            if (acceptChoice is null) {
+                Warn("Found no 'accept teachings' choice for story '{0}'", sname);
+                continue;
+            }
+            // Find what Law is this for:
+            var lawMatch = LawMatchRegex.Match(acceptChoice.OKResult);
+            if (!lawMatch.Success) {
+                Warn("Couldn't parse what Law story '{0}' is matching with!", sname);
+                continue;
+            }
+            var law = lawMatch.Groups[1].Value.Trim();
+            Info("Before:");
+            Info(acceptChoice.OKResult);
+            // It's easier to inject my own function than to figure out whether this can be done from the API.
+            AddCodeToStart(ref acceptChoice.OKResult, $"""
+            GameMain:GetMod("{nameof(MandateHelper)}").{nameof(MandateHookFor)}("{law}");
+            """);
+            Info("After:");
+            Info(acceptChoice.OKResult);
+        }
+    }
+    public static void MandateHookFor(string law) {
+        AddStoryMsg("yay! I'm working! woooo. called for " + law);
+    }
+    public static void AddStoryMsg(string format, params object[] args) {
+        GameDefine.AddTempDescLine(GameDefine.GetFixedStory(null, string.Format(format, args)));
+    }
 }
---
+++ src/Utils.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MandateHelper;
+public static class Utils {
+    public static string LongestCommonPrefix(string str1, string str2) {
+        int commonLength = 0;
+        while (commonLength < Math.Min(str1.Length, str2.Length) &&
+              str1[commonLength] == str2[commonLength]) {
+            commonLength++;
+        }
+        return str1.Substring(0, commonLength);
+    }
+    public static string CommonIndentation(string[] lines) {
+        if (lines.Length == 0) {
+            return "";
+        }
+        var prefix = lines.Aggregate(LongestCommonPrefix);
+        return new string(prefix.TakeWhile(x => char.IsWhiteSpace(x)).ToArray());
+    }
+    public static Regex IndentRegex = new(@"^(\s*)");
+    /// <summary>
+    /// Appends/prepends code, correctly handling indentation.
+    /// </summary>
+    public static void AddCodeTo(ref string to, string code, bool end = true) {
+        // Find indentation level of first/last nonempty line:
+        var origLines = to.Split('\n').Where(x => x.Any(c => !char.IsWhiteSpace(c)));
+        var nearestLine = end ? origLines.Last() : origLines.First();
+        string nearestIndent = "";
+        if (nearestLine is not null) {
+            nearestIndent = IndentRegex.Match(nearestLine).Groups[1].Value;
+        }
+        // Strip equal indentation, readd target one:
+        var codeClean = code.Trim('\n').Split('\n');
+        var common = CommonIndentation(codeClean);
+        var newLines = codeClean.Select(x => nearestIndent + x.Substring(common.Length));
+        // Finally, add the new lines:
+        if (end) {
+            to = to + "\n" + "\n".Join(newLines);
+        } else {
+            to = "\n".Join(newLines) + "\n" + to;
+        }
+    }
+    public static void AddCodeToStart(ref string to, string code) {
+        AddCodeTo(ref to, code, end: false);
+    }
+}
